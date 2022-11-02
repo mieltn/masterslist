1f0ce9afd3acf85be01ccf6453b4b29fba24b102
mieltn <mike.elyutin@gmail.com>
2022-11-02 19:45:39 +0500

    removed more old folders

--- main/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Programm, Country, Subject, Comment
-# Register your models here.
-admin.site.register(Programm)
-admin.site.register(Country)
-admin.site.register(Subject)
-admin.site.register(Comment)
