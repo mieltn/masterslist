9aea43b1878da1bb418976030a8ca902f4143f4c
mieltn <mike.elyutin@gmail.com>
2022-12-24 11:44:13 +0500

    added program link via icon

--- frontend/src/pages/Program.js
+++ frontend/src/pages/Program.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import '../Form.css'
+import linkIcon from '../static/icons/link.png'
 
 
 function Program() {
@@ -40,7 +41,13 @@ function Program() {
 
     return (
         <div className="Program">
-            <h1 style={{textAlign: "center"}}>{program.name}</h1>
+            <h1 style={{textAlign: "center"}}>
+                {program.name}
+                <a href={program.website}>
+                    <img src={linkIcon} width="20" height="20" style={{marginLeft: "10px"}}/>
+                </a>
+            </h1>
+            <h2 style={{textAlign: "center", color: "lightgrey"}}>{program.country}</h2>
             <div className="btn-toolbar">
                 <button className="btn btn-danger mult-btn" onClick={handleDelete}>delete</button>
                 <button className="btn btn-secondary mult-btn" onClick={handleEdit}>edit</button>
